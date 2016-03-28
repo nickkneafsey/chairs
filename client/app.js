e7cb09700862bc73846f39466ad12ef758fb040d
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2016-03-28 15:54:01 -0700

    Elements are circling

--- client/app.js
+++ client/app.js
@@ -4,9 +4,20 @@ chairApp.controller('ChairController', ['$scope', '$timeout', function($scope, $
   $scope.chairs=[];
 
   function num(numChair) {
+    var radius = 150;
+    var width = 400;
+    var height = 400;
+    var chairHeight = 10;
+    var chairWidth = 10; 
+    var angle = 0;
+    var step = (2*Math.PI) / numChair;
 
     for (var i = 1; i <= numChair; i++) {
-     $scope.chairs.push(i);
+      var chair = { number: i };
+      chair.x = Math.round(width/2 + radius * Math.cos(angle) - chairWidth/2);
+      chair.y = Math.round(height/2 + radius * Math.sin(angle) - chairHeight/2);
+      $scope.chairs.push(chair);
+      angle += step;
     }
 
     var currentIndex = 0;
